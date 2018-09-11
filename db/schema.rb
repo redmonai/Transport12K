3cdf0047106dce0c5d36135259895371634b2fb3
Ailbhe <redmonai@tcd.ie>
2018-09-11 17:32:23 +0100

    migrated to postgresql to prep for heroku deployment

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2018_09_03_161004) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "buses", force: :cascade do |t|
     t.string "route"
     t.string "destination"
